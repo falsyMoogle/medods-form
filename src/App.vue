4aafa85df8dec0f126ba2c07de5be328a3a5dccb
falsyMoogle <mgsmile1990@gmail.com>
2020-09-05 13:22:13 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<ClientForm />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ClientForm from "./components/ClientForm.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		ClientForm,
+	},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+	* {
+		box-sizing: border-box;
+		padding: 0;
+		margin: 0;
+	}
+	#app {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		padding-top: 1rem;
+	}
 </style>
---
+++ src/components/ClientForm.vue
@@ -0,0 +1,21 @@
+<template>
+	<form class="form"></form>
+</template>
+
+<script>
+export default {
+	name: "ClientForm",
+};
+</script>
+
+<style lang="scss" scoped>
+	.form {
+		width: 100%;
+		max-width: 600px;
+		min-height: 300px;
+		margin: 10px 15px;
+		padding: 1rem 0.8rem;
+		border-radius: 8px;
+		border: 1px solid #000;
+	}
+</style>
